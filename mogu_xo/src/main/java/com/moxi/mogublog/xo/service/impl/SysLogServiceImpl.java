08c422bc7976217a77e7ed352868c3423070af57
geshanzsq <497301391@qq.com>
2024-06-09 21:58:44 +0800

    feat(操作日志相关接口): 改造列表，使用 LambdaQueryWrapperPlus 减少 If 语句

--- mogu_xo/src/main/java/com/moxi/mogublog/xo/service/impl/SysLogServiceImpl.java
+++ mogu_xo/src/main/java/com/moxi/mogublog/xo/service/impl/SysLogServiceImpl.java
@@ -13,6 +13,7 @@ import com.moxi.mogublog.xo.service.SysLogService;
 import com.moxi.mogublog.xo.vo.SysLogVO;
 import com.moxi.mougblog.base.enums.EStatus;
 import com.moxi.mougblog.base.global.Constants;
+import com.moxi.mougblog.base.mybatis.plugin.query.LambdaQueryWrapperPlus;
 import com.moxi.mougblog.base.serviceImpl.SuperServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,39 +36,31 @@ public class SysLogServiceImpl extends SuperServiceImpl<SysLogMapper, SysLog> im
     @Override
     public IPage<SysLog> getPageList(SysLogVO sysLogVO) {
 
-        QueryWrapper<SysLog> queryWrapper = new QueryWrapper<>();
+        LambdaQueryWrapperPlus<SysLog> queryWrapper = new LambdaQueryWrapperPlus<>();
+        queryWrapper.eq(SysLog::getUserName, sysLogVO.getUserName().trim());
+        queryWrapper.eq(SysLog::getOperation, sysLogVO.getOperation());
+        queryWrapper.eq(SysLog::getIp, sysLogVO.getIp());
 
-        if (StringUtils.isNotBlank(sysLogVO.getUserName())) {
-            queryWrapper.eq(SQLConf.USER_NAME, sysLogVO.getUserName().trim());
-        }
-
-        if (StringUtils.isNotBlank(sysLogVO.getOperation())) {
-            queryWrapper.eq(SQLConf.OPERATION, sysLogVO.getOperation());
-        }
-
-        if (StringUtils.isNotBlank(sysLogVO.getIp())) {
-            queryWrapper.eq(SQLConf.IP, sysLogVO.getIp());
-        }
 
         if (StringUtils.isNotBlank(sysLogVO.getStartTime())) {
             String[] time = sysLogVO.getStartTime().split(SysConf.FILE_SEGMENTATION);
             if (time.length == Constants.NUM_TWO) {
-                queryWrapper.between(SQLConf.CREATE_TIME, DateUtils.str2Date(time[0]), DateUtils.str2Date(time[1]));
+                queryWrapper.between(SysLog::getCreateTime, DateUtils.str2Date(time[0]), DateUtils.str2Date(time[1]));
             }
         }
 
         if (StringUtils.isNotBlank(sysLogVO.getSpendTimeStr())) {
             String[] spendTimeList = StringUtils.split(sysLogVO.getSpendTimeStr(), Constants.SYMBOL_UNDERLINE);
             if (spendTimeList.length == Constants.NUM_TWO) {
-                queryWrapper.between(SQLConf.SPEND_TIME, Integer.valueOf(spendTimeList[0]), Integer.valueOf(spendTimeList[1]));
+                queryWrapper.between(SysLog::getSpendTime, Integer.valueOf(spendTimeList[0]), Integer.valueOf(spendTimeList[1]));
             }
         }
 
         Page<SysLog> page = new Page<>();
         page.setCurrent(sysLogVO.getCurrentPage());
         page.setSize(sysLogVO.getPageSize());
-        queryWrapper.eq(SQLConf.STATUS, EStatus.ENABLE);
-        queryWrapper.orderByDesc(SQLConf.CREATE_TIME);
+        queryWrapper.eq(SysLog::getStatus, EStatus.ENABLE);
+        queryWrapper.orderByDesc(SysLog::getCreateTime);
         IPage<SysLog> pageList = sysLogService.page(page, queryWrapper);
         return pageList;
     }

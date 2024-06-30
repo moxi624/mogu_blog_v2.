efefc6dac1c2daa60b4668809674c77d0df6bd77
陌溪 <xzx19950624@qq.com>
2024-06-30 23:56:44 +0800

    feat: 更新一键部署脚本

--- doc/docker-compose/bin/replaceIp.py
+++ doc/docker-compose/bin/replaceIp.py
@@ -1,15 +1,55 @@
 # -*- coding:utf-8 -*-
-from urllib2 import urlopen
+
+import socket
+import subprocess
+import re
 import io
-import re
-# 获取ip地址
+def is_private_ip(ip):
+    private_ip_ranges = [
+        re.compile(r'10\.\d+\.\d+\.\d+'),
+        re.compile(r'127\.\d+\.\d+\.\d+'),
+        re.compile(r'172\.(1[6-9]|2\d|3[01])\.\d+\.\d+'),
+        re.compile(r'192\.168\.\d+\.\d+')
+    ]
+    for pattern in private_ip_ranges:
+        if pattern.match(ip):
+            return True
+    return False
 
+def get_ip():
+    p = subprocess.Popen("ifconfig", stdout=subprocess.PIPE)
+    output = p.stdout.read()
+    pattern = re.compile(r'inet (\d+\.\d+\.\d+\.\d+)')
+    ips = pattern.findall(output)
+    public_ips = [ip for ip in ips if not is_private_ip(ip)]
+    private_ips = [ip for ip in ips if is_private_ip(ip)]
+    private_ips.sort(key=lambda x: x.startswith('192.'), reverse=True)
+    sorted_ips = public_ips + private_ips
+    return sorted_ips
 
-import requests
-import re
- 
-myIp = requests.get('https://checkip.amazonaws.com').text.strip()
-print "外网IP：", myIp
+ips = get_ip()
+
+print("当前系统的IP地址列表：")
+print(ips)
+
+ip_address = ips[0]
+def is_private_ip(ip):
+    ip_parts = ip.split('.')
+    if ip_parts[0] == '10':
+        return True
+    elif ip_parts[0] == '172' and 16 <= int(ip_parts[1]) <= 31:
+        return True
+    elif ip_parts[0] == '192' and ip_parts[1] == '168':
+        return True
+    elif ip_parts[0] == '127':
+        return True
+    else:
+        return False
+print("系统选择IP地址:" + ip_address)
+if is_private_ip(ip_address):
+    print("该IP地址运行在局域网")
+else:
+    print("该IP地址运行在云服务器")
 
 # 替换ip地址
 def replace(file, newStr):
@@ -31,6 +71,5 @@ def replace(file, newStr):
     with io.open(file,"w",encoding="utf-8") as f:
         f.write(fileData)
 
-replace("../config/vue_mogu_admin.env", myIp)
-replace("../config/vue_mogu_web.env", myIp)
-
+replace("../config/vue_mogu_admin.env", ip_address)
+replace("../config/vue_mogu_web.env", ip_address)

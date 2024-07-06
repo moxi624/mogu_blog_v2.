2fbc469c739b00839cb1ab971ffef435c95ad3b7
Wuzeyue020829 <1109607743@qq.com>
2024-07-06 22:13:33 +0800

    修改拼写错误

--- mogu_admin/src/main/java/com/moxi/mogublog/admin/annotion/AuthorityVerify/AuthorityVerifyAspect.java
+++ mogu_admin/src/main/java/com/moxi/mogublog/admin/annotion/AuthorityVerify/AuthorityVerifyAspect.java
@@ -92,9 +92,9 @@ public class AuthorityVerifyAspect {
 
             Role role = roleService.getById(roleUid);
 
-            String caetgoryMenuUids = role.getCategoryMenuUids();
+            String categoryMenuUids = role.getCategoryMenuUids();
 
-            String[] uids = caetgoryMenuUids.replace("[", "").replace("]", "").replace("\"", "").split(",");
+            String[] uids = categoryMenuUids.replace("[", "").replace("]", "").replace("\"", "").split(",");
 
             List<String> categoryMenuUids = new ArrayList<>(Arrays.asList(uids));
 
